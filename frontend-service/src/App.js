d9e01302a0a4720a661201d48ea332de0f03270d
Pablo Heredia <pheredia2004@gmail.com>
2024-06-06 22:32:38 -0600

    Eliminado paginas que ni al caso, funcionalidad basica de ruteo. Añadir auth y conexiones al back

--- frontend-service/src/App.js
+++ frontend-service/src/App.js
@@ -2,33 +2,24 @@ import "./normal.css";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import LoginForm from "./components/login/LoginForm";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  /*
   const { currentUser } = useContext(AuthContext);
 
   const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="auth/login" />;
+    return currentUser ? children : <Navigate to="auth" />;
   };
+  */
 
   return (
     <div className="App">
       <Routes>
-        <Route
-          index
-          exact
-          path="/"
-          element={
-            <RequireAuth>
-              <Home />
-            </RequireAuth>
-          }
-        />
-        <Route exact path="auth/login" element={<Login />} />
-        <Route exact path="login" element={<LoginForm />} />
+        <Route exact path="/" element={<Navigate to="/auth" />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/auth" element={<Login />} />
       </Routes>
     </div>
   );
